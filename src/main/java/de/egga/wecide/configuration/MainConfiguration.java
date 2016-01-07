19e29b3f28f4269e31ad39ed16dde029c2aa560f
Egga (Jan Hartung) <github@egga.de>
2016-01-07 23:46:35 +0100

    add database

--- src/main/java/de/egga/wecide/configuration/MainConfiguration.java
+++ src/main/java/de/egga/wecide/configuration/MainConfiguration.java
@@ -2,7 +2,14 @@ package de.egga.wecide.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 /**
  * @author egga
@@ -13,5 +20,15 @@ public class MainConfiguration {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MainConfiguration.class, args);
+    }
+
+    @Configuration
+    public class DatabaseConfig {
+        @Bean
+        @Primary
+        @ConfigurationProperties(prefix = "spring.datasource")
+        public DataSource dataSource() {
+            return DataSourceBuilder.create().build();
+        }
     }
 }
