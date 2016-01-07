829515419e6899523fc2a73b46dcc4f0c8a4871c
Egga (Jan Hartung) <github@egga.de>
2016-01-07 23:05:58 +0100

    add config and swagger stuff

---
+++ src/main/java/de/egga/wecide/configuration/MainConfiguration.java
@@ -0,0 +1,17 @@
+package de.egga.wecide.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author egga
+ */
+@ComponentScan(basePackages = "de.egga.wecide")
+@EnableAutoConfiguration
+public class MainConfiguration {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MainConfiguration.class, args);
+    }
+}
---
+++ src/main/java/de/egga/wecide/configuration/SwaggerConfiguration.java
@@ -0,0 +1,36 @@
+package de.egga.wecide.configuration;
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.models.dto.ApiInfo;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author egga
+ */
+@Configuration
+@EnableSwagger
+public class SwaggerConfiguration {
+    private SpringSwaggerConfig springSwaggerConfig;
+
+    @Autowired
+    public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
+        this.springSwaggerConfig = springSwaggerConfig;
+    }
+
+    @Bean
+    public SwaggerSpringMvcPlugin customImplementation() {
+        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig).apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "BootKick API",
+                "Kickstarting APIs with Spring Boot",
+                null, null, null, null
+        );
+    }
+}
